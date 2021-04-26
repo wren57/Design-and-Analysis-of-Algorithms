37d0382a23b3839bdc986572158ef652c2e8bff5
WangJiayi <3097710242@qq.com>
2021-04-26 15:48:53 +0800

    ex08-1 矩阵链乘法

---
+++ src/ex08/RecurMatrixChain.java
@@ -0,0 +1,35 @@
+package ex08;
+
+import java.util.Scanner;
+
+/**
+ * @author WangJiayi
+ * @since 2021-04-26
+ */
+public class RecurMatrixChain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] p = new int[n + 1];
+        int[][] m = new int[n + 1][n + 1];
+        int[][] s = new int[n + 1][n + 1];
+        for (int i = 0; i <= n; ++i) {
+            p[i] = scanner.nextInt();
+        }
+        for (int r = 2; r <= n; ++r) {
+            for (int i = 1; i <= n - r + 1; ++i) {
+                int j = i + r - 1;
+                m[i][j] = m[i + 1][j] + p[i - 1] * p[i] * p[j];
+                s[i][j] = i;
+                for (int k = i + 1; k <= j - 1; ++k) {
+                    int t = m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j];
+                    if (t < m[i][j]) {
+                        m[i][j] = t;
+                        s[i][j] = k;
+                    }
+                }
+            }
+        }
+        System.out.println(m[1][n]);
+    }
+}
